e3a6ce103bc430509e5e9e180b35a76b5f44eb58
@hexlet/cli <support@hexlet.io>
2024-12-08 17:01:54 +0300

    submit java-web-ru/mvc

--- java-web-ru/mvc/src/main/java/exercise/controller/PostsController.java
+++ java-web-ru/mvc/src/main/java/exercise/controller/PostsController.java
@@ -63,36 +63,38 @@ public class PostsController {
     // BEGIN
     public static void edit(Context ctx) {
         var id = ctx.pathParamAsClass("id", Long.class).get();
-        var post = PostRepository.find(id).orElseThrow(() -> new NotFoundResponse("Post not found"));
+
+        var post = PostRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Post not found"));
+
         var page = new EditPostPage(id, post.getName(), post.getBody(), null);
         ctx.render("posts/edit.jte", Collections.singletonMap("page", page));
     }
 
-
     public static void update(Context ctx) {
         var id = ctx.pathParamAsClass("id", Long.class).get();
-        var name = ctx.formParam("name");
-        var body = ctx.formParam("body");
 
-        Post post = PostRepository.find(id).orElseThrow(() -> new NotFoundResponse("Post not found"));
+        var post = PostRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Post not found"));
         try {
-            ctx.formParamAsClass("name", String.class)
-                    .check(value -> value.length() > 2, "Название не должно быть короче двух символов")
-                    .check(value -> PostRepository.getEntities().stream()
-                                    .noneMatch(art -> art.getName().equals(value)),
-                            "Статья с таким названием уже существует")
+            var name = ctx.formParamAsClass("name", String.class)
+                    .check(value -> value.length() >= 2, "Название не должно быть короче двух символов")
                     .get();
-            ctx.formParamAsClass("body", String.class)
-                    .check(value -> value.length() >= 10,
-                            "Статья должна быть не короче 10 символов")
+
+            var body = ctx.formParamAsClass("body", String.class)
+                    .check(value -> value.length() >= 10, "Пост должен быть не короче 10 символов")
                     .get();
-            post.setName(name);
+
             post.setBody(body);
+            post.setName(name);
             PostRepository.save(post);
+
             ctx.redirect("/posts");
         } catch (ValidationException e) {
+            var name = ctx.formParam("name");
+            var body = ctx.formParam("body");
             var page = new EditPostPage(id, name, body, e.getErrors());
-            ctx.status(422).render("posts/edit.jte", Collections.singletonMap("page", page));
+            ctx.render("posts/edit.jte", Collections.singletonMap("page", page)).status(422);
         }
     }
     // END
