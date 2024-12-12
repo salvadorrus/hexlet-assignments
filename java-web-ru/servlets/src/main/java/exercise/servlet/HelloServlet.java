0f65b24d09b82cb9292aef713bb806593ada4a64
@hexlet/cli <support@hexlet.io>
2024-12-12 15:56:40 +0300

    submit java-web-ru/servlets

--- java-web-ru/servlets/src/main/java/exercise/servlet/HelloServlet.java
+++ java-web-ru/servlets/src/main/java/exercise/servlet/HelloServlet.java
@@ -19,7 +19,7 @@ public class HelloServlet extends HttpServlet {
             name = "Guest";
         }
 
-        String message = "Hello, " + name + "!";
+        String message = String.format("Hello, " + name + "!");
         req.setAttribute("message", message);
         req.getRequestDispatcher("/WEB-INF/users.jsp").forward(req, resp);
     }
