8232e862f2ff975df1fdff537a64a4234221c176
@hexlet/cli <support@hexlet.io>
2024-12-12 15:37:23 +0300

    submit java-web-ru/servlets

--- java-web-ru/servlets/src/main/java/exercise/servlet/HelloServlet.java
+++ java-web-ru/servlets/src/main/java/exercise/servlet/HelloServlet.java
@@ -13,12 +13,14 @@ public class HelloServlet extends HttpServlet {
     // BEGIN
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String message1 = "Hello, ";
+        String name = String.valueOf(req);
+        String message1 = "Hello, " + name + "!";
         String message2 = "Hello, Guest!";
-        String[] name = {"Jonn"};
-        req.setAttribute("message", message1);
-        req.setAttribute("name", name);
-
+        if (name == null) {
+            req.setAttribute("message", message2);
+        } else {
+            req.setAttribute("message", message1);
+        }
         req.getRequestDispatcher("/WEB-INF/users.jsp").forward(req, resp);
     }
     // END
