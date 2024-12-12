12ce320c1f6c43d44aa7ef90bda4dd3c31c1be93
@hexlet/cli <support@hexlet.io>
2024-12-12 16:21:52 +0300

    submit java-web-ru/servlets

--- java-web-ru/servlets/src/main/java/exercise/servlet/HelloServlet.java
+++ java-web-ru/servlets/src/main/java/exercise/servlet/HelloServlet.java
@@ -14,7 +14,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         var name = req.getParameter("name");
-        if (name.isEmpty()) {
+        if (name == null) {
             name = "Guest";
         }
 
